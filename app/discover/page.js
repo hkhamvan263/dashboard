96f94c6ebfebd3ba4f32700b671afcee5073af29
mpshohan <mdpatwary377@gmail.com>
2024-09-08 01:30:01 -0400

    changes to discover page

---
+++ app/discover/page.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Home(){
+    return (
+        <Box
+            marginTop={20}
+        >
+        <Typography variant="h2" textAlign={'center'}>Coming Soon....</Typography>
+        </Box>
+    )
+}
